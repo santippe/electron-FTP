5201480ea6ab69f407651e3bc67cebd495d3b7b0
Marco Mascioli <5377295+santippe@users.noreply.github.com>
2019-02-22 17:59:45 +0100

    updates

--- scripts/ftp.js
+++ scripts/ftp.js
@@ -1,35 +1,54 @@
 let configFTP = {
-    'host': 'peshop.servehttp.com',
-    'port': 6000,
-    'user': 'ftppeshop',
-    'password': 'porcodio'
+    'host': 'localhost',
+    'port': 21,
+    'user': 'mascioli',
+    'password': 'test',
+    'startDirectory': '/',
 };
 let listOFEntries = [];
 const { dialog } = require('electron').remote;
-function loadFileLocal() {    
+function loadFileLocal() {
     let fileSelected = dialog.showOpenDialog();
     for (let a = 0; a < fileSelected.length; a++) {
         dialog.showMessageBox({ 'message': fileSelected[a] });
+    }
+}
+function createNewFile() {
+    let localpath = 'filename';
+    createWriteStream('./download/' + localpath);
+    //cycle for all the filenames
+    let arrNames = [];
+    for (let arrName in arrNames) {
+        let tmpRemotePAth = configFTP.startDirectory + arrName + "csv";
+        downloadFromFtp(remote, localpath);
     }
 }
 function downloadFromFtp(remotepath, localpath) {
     genericFTPCommand((c) => {
         c.on('ready', function () {
             c.get(remotepath, function (err, stream) {
-                if (err) throw err;
-                stream.once('close', function () { c.end(); });
-                stream.pipe(fs.createWriteStream(__dir__ + '/download/' + localpath));
+                if (err) {
+                    dialog.showMessageBox(`File ${localpath} not downloaded`);
+                    console.log(err);
+                } else {
+                    stream.once('close', function () {
+                        c.end();
+                        dialog.showMessageBox(`File ${localpath} successfully donwloaded`);
+                    });
+                    stream.pipe(fs.createWriteStream('./download/' + localpath));
+                }
             });
         });
-    });        
+    });
 }
-function enumerateEntiresFromFtp(remotepath) {
+function enumerateEntiresFromFtp() {
     let outputarr = [];
+    let remotepath = configFTP.startDirectory;
     genericFTPCommand((c) => {
         c.on('ready', function () {
             c.list(remotepath, function (err, listArray) {
                 if (err) throw err
-                else{
+                else {
                     listOFEntries = listArray;
                     dialog.showMessageBox({ 'message': 'Connection OK' });
                 }
@@ -39,12 +58,12 @@ function enumerateEntiresFromFtp(remotepath) {
                 // });                    
             });
         });
-    });        
+    });
     return outputarr;
 }
 function genericFTPCommand(actionToDo) {
     const ftpClient = require('ftp');
-    let c = new ftpClient();    
+    let c = new ftpClient();
     actionToDo(c);
     c.connect(configFTP);
 }
